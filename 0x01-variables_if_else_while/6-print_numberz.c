f98b35018efdabef673d359a2f7c4f967bca35e9
brianlov3scode <brianchiira18@gmail.com>
2023-08-14 16:54:01 -0700

    A program that prints single-digit numbers in base 10, starting from 0, without using any variable of type char and using putchar only twice

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers of base 10 from 0 to 9.
+ * Only the putchar function is used (twice).
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0' + 0);
+	putchar('0' + 1);
+	putchar('0' + 2);
+	putchar('0' + 3);
+	putchar('0' + 4);
+	putchar('0' + 5);
+	putchar('0' + 6);
+	putchar('0' + 7);
+	putchar('0' + 8);
+	putchar('0' + 9);
+
+	putchar('\n');
+
+	return (0);
+}
+
