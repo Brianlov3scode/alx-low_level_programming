1ac0dfe2f2e8d93380f952dcdd6bae5b927aa9ea
brianlov3scode <brianchiira18@gmail.com>
2023-08-17 16:43:37 -0700

    a function that checks for uppercase characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,20 @@
-#include <unistd.h>
-
-void _putchar(char c);
-
+#include <main.h>
 /**
- * main - Entry point
- *
- * Description: Prints "_putchar" followed by a new line
- * using the _putchar function
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
-
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to be written
- *
- * Return: On success 1. On error, -1 is returned,
- * and errno is set appropriately.
- */
-void _putchar(char c)
-{
-    write(1, &c, 1);
-}
-
